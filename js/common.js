cd35b3de870fa041d7876de03507cdc61ae5b239
ANDyouNo <younofl312w@gmail.com>
2023-09-21 14:41:02 +0300

    f

--- js/common.js
+++ js/common.js
@@ -1,1 +1,46 @@
-// $('body').hide()
+const donatButton = document.querySelector(".header__button")
+
+donatButton.addEventListener('click', function() {
+  const delayInMilliseconds = 1000; //1 second
+  donatButton.innerHTML = "Thanks" + '<img src="./images/heart.svg" alt="heart" />'
+  setTimeout(function() {
+    //your code to be executed after 1 second
+    document.location = 'https://www.apple.com/'
+  }, delayInMilliseconds);
+})
+
+
+
+const box = document.querySelector('.box')
+
+box.addEventListener('mouseover', () => {
+  box.classList.add('red')
+})
+
+box.addEventListener('mouseout', () => {
+  box.classList.remove('red')
+})
+
+const submenuItems = document.querySelectorAll('.submenu')
+const headerLinks = document.querySelectorAll('.link-container')
+
+console.log(headerLinks)
+
+headerLinks.forEach((item, index) => {
+  item.addEventListener('mouseover', () => {
+    submenuItems.forEach((item) => {
+      item.classList.add('hide')
+      item.classList.remove('submenu')
+    })
+    submenuItems[index].classList.remove('hide')
+    submenuItems[index].classList.add('submenu')
+  })
+})
+
+submenuItems.forEach((item) => {
+  item.addEventListener('mouseout', () => {
+    item.classList.add('hide')
+    item.classList.remove('submenu')
+    console.log('hide')
+  })
+})
